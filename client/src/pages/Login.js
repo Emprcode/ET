17ab23a6c68f863b3c5225f0f9599b0b2d045e45
Narayan Aryal <aryal.coding@gmail.com>
2023-02-28 12:31:11 +1100

    register and login page ui completed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { CustomForm } from "../components/customFields/CustomForm";
+import { MainLayout } from "../components/layout/MainLayout";
 
 const Login = () => {
+  const inputs = [
+    {
+      label: "Email",
+      type: "email",
+      name: "email",
+      required: true,
+      placeholder: "email@email.com",
+    },
+    {
+      label: "Password",
+      type: "password",
+      name: "password",
+      required: true,
+      placeholder: "******",
+    },
+  ];
   return (
-    <div>Login</div>
-  )
-}
+    <MainLayout>
+      <Container>
+        <Row className="p-5">
+          <Col className="info bg-primary rounded d-flex justify-content-center align-items-center">
+            <div className="text-light text-center  ">
+              <h3 className="p-3">Welcome to our system</h3>
+              <p>Login to use our system and manage your financial situation</p>
+            </div>
+          </Col>
+          <Col>
+            <Form>
+              <h3>Login</h3>
+              <hr />
+              {inputs.map((item, i) => (
+                <CustomForm key={i} {...item} />
+              ))}
 
-export default Login
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </MainLayout>
+  );
+};
+
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,84 @@
-import React from 'react'
-import { MainLayout } from '../components/layout/MainLayout'
+import React, { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { CustomForm } from "../components/customFields/CustomForm";
+import { MainLayout } from "../components/layout/MainLayout";
+import {toast} from 'react-toastify'
 
-const Register = () => {
+const Login = () => {
+  const [form, setForm] = useState({});
+
+  const handleOnChange = e => {
+    const {name, value} = e.target
+
+    setForm({
+      ...form, [name]: value
+    })
+  }
+
+  const handleOnSubmit = async e=> {
+    e.preventDefault()
+  //   const {status, message} = await postUser(form)
+
+  //   toast[status](message)
+  }
+
+  const inputs = [
+    {
+      label: "Username",
+      type: "text",
+      name: "name",
+      required: true,
+      placeholder: "abced",
+    },
+    {
+      label: "Email",
+      type: "email",
+      name: "email",
+      required: true,
+      placeholder: "email@email.com",
+    },
+    {
+      label: "Password",
+      type: "password",
+      name: "password",
+      required: true,
+      placeholder: "******",
+    },
+    {
+      label: "Confirm Password",
+      type: "password",
+      name: "confirmPassword",
+      required: true,
+      placeholder: "******",
+    },
+  ];
   return (
-    <MainLayout>Register</MainLayout>
-  )
-}
+    <MainLayout>
+      <Container>
+        <Row className="p-5">
+          <Col className="info bg-primary rounded d-flex justify-content-center align-items-center">
+            <div className="text-light text-center  ">
+              <h3 className="p-3">Welcome to our system</h3>
+              <p>Register here to use our system</p>
+            </div>
+          </Col>
+          <Col>
+            <Form onSubmit={handleOnSubmit}>
+              <h3>Register</h3>
+              <hr />
+              {inputs.map((item, i) => (
+                <CustomForm key={i} {...item} onChange={handleOnChange}/>
+              ))}
 
-export default Register
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </MainLayout>
+  );
+};
+
+export default Login;
